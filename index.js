6e091e0534bd27b663396fb6280db15282a2b789
AnanyAbellop <padronalbany@gmail.com>
2020-07-27 23:59:21 +0000

    Ya tiene las letras A,J,Q,K y ademas las letras correspondientes cambian a rojo

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ window.onload = ()=>{
     //Alerta para saber si está funcionando alert("Hola estoy aqui!");
     //Lógica para los números de las tarjetas
     let num =Math.floor((Math.random()*13)+1);
-      
+
+    //Lógica si es A.J.Q.K
+    if(num==1)
+      {
+          num="A";
+      }else if(num==11){
+          num="J";
+      }else if(num==12){
+          num="Q";
+      }else if(num==13) {
+          num="K";
+      }
+
     let pinta=Math.floor((Math.random()*4)+1);
     
     //Asignacion de la pinta y figura
@@ -10,9 +22,9 @@ window.onload = ()=>{
     let dibujo="@";
         if (pinta==1){
             dibujo="❤️";
-            id="#heart";
+            id="heart";
         }else if(pinta==2){
-            id="#spade";
+            id="spade";
             dibujo="♠️";
         }else if (pinta==3)
         {
@@ -26,19 +38,18 @@ window.onload = ()=>{
     
     //Cuerpo de la Carta
     let card =document.createElement("div");
-    card.classList.add("d-flex","flex-nowarp","container","justify-content-center");
+    card.classList.add("d-flex","flex-nowarp","container","justify-content-center","rounded");
     card.style.width="250px";
     card.style.height="290px";
     card.style.border="solid 5px black";
     document.body.appendChild(card);
     card.innerHTML=`
-            <div class="card  border-0 mb-3" style="max-width: 18rem;">
-                <div class="card-header bg-transparent border-0 id="${id}" justify-content-start "> ${dibujo}</div>
-                <div class="card-body border-0">
+            <div class="card  border-0 mb-3 carta d-flex" >
+                <div class="card-header  border-0 p-2 bg-transparent ${id}" > ${dibujo}</div>
+                <div class="card-body border-0  d-flex align-items-center ${id} ">
                     <h1 class="card-title ">${num}</h1>
-                    
                  </div>
-                <div class="card-footer bg-transparent border-0 "></div>
+                <div class="card-footer border-0 ${id}  bg-transparent ">${dibujo}</div>
             </div>`
 
 }
